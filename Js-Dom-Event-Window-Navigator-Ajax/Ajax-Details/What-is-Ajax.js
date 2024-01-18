2d5e11fe2544d93593037df0313cd79811fe0cba
Riajul Islam <riajulpislam@gmail.com>
2024-01-18 19:04:00 +0600

    js-dom

---
+++ Js-Dom-Event-Window-Navigator-Ajax/Ajax-Details/What-is-Ajax.js
@@ -0,0 +1,40 @@
+//What is Ajax ?
+//Ajax full  from->(Asynchronous Javascript XML)
+
+//Javascript er akta Features hocche Ajax. seei features ta use kore korte pari (update a web page without
+//reloading the  page.)
+//webpage ke load na koreei oi webpage theke server a request pathate pari and respons korte pari.ajonne aetake bola hoy Developers Dream.
+//seei request hote pare getrequest,post,put,delete request.
+//Ajax use kore amra jee request pathabo, javascript er jee feature, feature tar main feature ta seta holo 
+//XMLHttpRequest obj
+
+
+/********************* What is Backend Api *************************/ 
+
+//Akta Software/ Web Application er 2ta part thake.Web Application a jokhon akta user use kore,tokhon browser a  jei part ta load hoy,sei part ta ke boli Web-Application er (clint-side). kinto web-Application er akta Backend part thakee,sei Backend part ta kokhono browser a load hoy na,oi part ta Server a chole/ba sei part ta ki boli (Backend/Server-side).
+
+//MERN-Stack a Software er (Clint-side) and (Server-side) Communication kore.
+//Communication korar somoy (clint-side) browser theke Server a akta request jai, Server seei request ta respons kore,clint er kase akta respons pathai.
+
+//MERN-Stack er full-stack statagi ke request-respons model boli. mern-stack er sobida holo 2ta teei javascript(clint-side(Frontend) and server-side(Backend)).
+
+//Akhon server-side and clint-side er akta communication hoy, ae communication hobar jeei process ta, seei process ta jar maddome hoy, orthat je request pathai and respons kore, takei boli Ajax.
+
+//Backend a request pathaia  Ajax practices korbo, setai Backend Api.
+
+//Backend System/Api create korar jonno akta tools er dorkar koy,tar name holo (Postman)
+
+//Build in XMLHttpRequest feature holo (old version) je feature deye Ajax er kaj ta kora jai.
+//Build in Fetch holo(letest version)
+
+//Ajax Packages
+//(1)-> Axios package(letest)(library)
+//(2)-> Jquery Axios(old)
+
+//Server a request pathate gele akta (URL) lagebe.Fetch and Axios deye URL pathabo.
+
+//request pathanor jonno best way hocche:
+//(1)-> Async-Await kora
+//(2)-> promises deye o pathate pari.
+
+//Api object er bitore string thakee, browser a stringify kore dekhbo->Json.stringify()
---
+++ Js-Dom-Event-Window-Navigator-Ajax/Js-Dom/Introduction-dom.js
@@ -0,0 +1,9 @@
+// DOM -> Document Object Model
+//When a web page is loaded,the browser creates a document object model of the page With the html dom. js can accsee and change all the element of an html document.
+
+//implement-> akta browser jokhon akta webpage ke load kore, tokhon  browser dom browser er webpage create kore fele.pore dom bivinno feature use kore amra onek doroner kaj korte pari.
+
+// amra  (Dom viewer plugin) ta install korbo browser a. ae dom viewer plugin ta browser er bitore je dom create hocche seta dekhai.
+
+//akta website jodi React a kora hoy, kinto code golo likhbo JSX a. Not a HTML. seei JSX tae browser a ashe HTML a convert hoyese. tokhon seei browser HTML er songee Javascript ke intrect  korbe. Browser HTML ke chinar jonno tar sathe Javascript er je akta interctiviti ase seta gotanor jonno  HTML Document ta ke browser nejar moto kore sajeye neii.setaeii DOM.
+//DOM er library holo (Jqury)
